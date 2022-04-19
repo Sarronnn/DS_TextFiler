d4ba0afa1a0d56912fe8eb23a4c42fb344856344
Sarron <stadesse@lion.lmu.edu>
2022-04-19 12:53:08 -0700

    6/8 unit tests passed

--- Homework3Eclipse/src/main/textfiller/TernaryTreeTextFiller.java
+++ Homework3Eclipse/src/main/textfiller/TernaryTreeTextFiller.java
@@ -5,14 +5,18 @@ import java.util.*;
 /**
  * A ternary-search-tree implementation of a text-autocompletion
  * trie, a simplified version of some autocomplete software.
- * @author YOUR_NAME_HERE
+ * @author SARRON_TADESSE
  */
 public class TernaryTreeTextFiller implements TextFiller {
 
     // Fields
     // -----------------------------------------------------------
+  
     private TTNode root;
-    private int size;
+   	private int size;
+   	
+
+   	
     
     // Constructor
     // -----------------------------------------------------------
@@ -22,37 +26,57 @@ public class TernaryTreeTextFiller implements TextFiller {
     }
     
     
+    
     // Methods
     // -----------------------------------------------------------
     
     public int size () {
-        throw new UnsupportedOperationException();
+        return getSize(root);
     }
 
     public boolean empty () {
-        throw new UnsupportedOperationException();
+        if (root != null) {
+        	return true;
+        }else {
+        	return false;
+        }
     }
     
     public void add (String toAdd) {
-        throw new UnsupportedOperationException();
+    	if (!this.contains(normalizeTerm(toAdd))) {
+             this.root = this.add(this.root, normalizeTerm(toAdd), 0);
+        }
     }
     
-    public boolean contains (String query) {
-        throw new UnsupportedOperationException();
+    public boolean contains (String word) {
+    	return this.contains(this.root, normalizeTerm(word));
     }
     
-    public String textFill (String query) {
-        throw new UnsupportedOperationException();
+    public String textFill (String word) {
+    	return this.textFill(this.root, normalizeTerm(word), "");
     }
     
-    public List<String> getSortedList () {
-        throw new UnsupportedOperationException();
+    public List<String> getSortedList() {
+       ArrayList<String> sortedList = new ArrayList<>();
+       if (this.empty()) {
+    	   return sortedList;
+       }
+       this.getSortedList(this.root, "", sortedList);
+       return sortedList;
     }
     
     
     // Private Helper Methods
     // -----------------------------------------------------------
-    
+
+    /**
+     * maybe convert to array list??
+     * @param add
+     */
+   
+    	// What if root is not empty?? We compare the first character of the new word with that of root and decide where is goes
+    	
+    	
     /**
      * Normalizes a term to either add or search for in the tree,
      * since we do not want to allow the addition of either null or
@@ -85,7 +109,178 @@ public class TernaryTreeTextFiller implements TextFiller {
     }
     
     // [!] Add your own helper methods here!
+    private int getSize(TTNode node) {
+    	return size;
+    }
     
+    private String textFill(TTNode node, String word, String other) {
+        if (node == null) {
+            return null;
+        }
+        int compare = compareChars(word.charAt(0), node.letter);
+        if (compare == 0) {
+            if (word.length() == 1) {
+                return suffix(node, other);
+            }
+            other += node.letter;
+            return textFill(node.mid, word.substring(1), other);
+        } 
+        else if (compare > 0) {
+            return textFill(node.right, word, other);
+        } 
+        else {
+            return textFill(node.left, word, other);
+        }
+    }
+
+    private String suffix(TTNode node, String word) {
+        word += node.letter;
+        return node.wordEnd ? word : suffix(node.mid, word);
+    }
+
+    public void add(String toAdd, int priority) {
+        toAdd = normalizeTerm(toAdd);
+        if (!this.contains(toAdd)) {
+            this.root = this.add(this.root, toAdd, priority);
+        }
+    }
+    
+    public String fullWord(String word) {
+        return this.fullWord(this.root, normalizeTerm(word), "");
+    }
+
+    private TTNode add(TTNode node, String toAdd, int queue) {
+        if (node == null) {
+            return addSuffix(node, toAdd, queue);
+        }
+        int compare = compareChars(toAdd.charAt(0), node.letter);
+        if (compare == 0) {
+            if (node.queue < queue) {
+                node.queue = queue;
+            }
+            if (toAdd.length() == 1 && !node.wordEnd) {
+                this.size++;
+                node.wordEnd = true;
+                return node;
+            }
+            node.mid = add(node.mid, toAdd.substring(1), queue);
+        } else if (compare > 0) {
+            node.right = add(node.right, toAdd, queue);
+        } else {
+            node.left = add(node.left, toAdd, queue);
+        }
+        return node;
+    }
+
+
+
+
+    /**
+     * Recursively searches the tree for the highest priority String
+     * that contains the incoming term as a prefix
+     * 
+     * @param n the current node
+     * @param query the prefix to search for
+     * @param str the current String that contains the already collected letters
+     * 
+     * @return the highest priority String that contains query as a prefix, null if no such term exists
+     */
+    private String fullWord(TTNode node, String word, String other) {
+        if (node == null) {
+            return null;
+        }
+        int compare = compareChars(word.charAt(0), node.letter);
+        if (compare < 0) {
+            return fullWord(node.left, word, other);
+        } else if (compare > 0) {
+            return fullWord(node.right, word, other);
+        } else {
+            other += node.letter;
+            if (word.length() == 1) {
+                return node.mid == null ? other : suffix(node.mid, other, node.queue);
+            }
+            return fullWord(node.mid, word.substring(1), other);
+        }
+    }
+
+    /**
+     * Recursively collects suffix with highest priority starting at incoming node
+     * 
+     * @param n the current node
+     * @param str the current String that contains already collected letters
+     * @param priority the highest priority encountered so far
+     * 
+     * @return a String that contains term with highest priority
+     */
+    private String suffix(TTNode node, String word, int queue) {
+        if (node.queue > queue) {
+            queue = node.queue;
+        }
+        if (node.left != null) {
+            word = suffix(node.left, word, queue);
+            if (node.left.queue >= queue) {
+                return word;
+            }
+        }
+        if (node.right != null) {
+            word = suffix(node.right, word, queue);
+            if (node.right.queue >= queue) {
+                return word;
+            }
+        }
+        if (node.queue < queue) {
+            return word;
+        }
+        if (node.mid != null) {
+            return suffix(node.mid, word + node.letter, queue);
+        }
+        return word + node.letter;
+    }
+    
+    private TTNode addSuffix(TTNode node, String suffix, int queue) {
+        if (suffix.length() <= 0) {
+            return null;
+        }
+        node = new TTNode(suffix.charAt(0), suffix.length() == 1, queue);
+        if (suffix.length() == 1) {
+            this.size++;
+        } else {
+            node.mid = addSuffix(node.mid, suffix.substring(1), queue);
+        }
+        return node;
+    }
+    private boolean contains(TTNode node, String word) {
+        if (node == null) {
+            return false;
+        }
+        int compare = compareChars(word.charAt(0), node.letter);
+        if (compare == 0) {
+        	if (word.length() == 1) {
+                return node.wordEnd;
+        	}
+        	return contains(node.mid, word.substring(1));
+        } 
+        else if (compare > 0) {
+            return contains(node.right, word);
+        } 
+        else {
+            return contains(node.left, word);
+        }
+    }
+    private void getSortedList(TTNode node, String word, ArrayList<String> collect) {
+        if (node.left != null) {
+            getSortedList(node.left, word, collect);
+        }
+        if (node.wordEnd) {
+            collect.add(word + node.letter);
+        }
+        if (node.mid != null) {
+            getSortedList(node.mid, word + node.letter, collect);
+        }
+        if (node.right != null) {
+            getSortedList(node.right, word, collect);
+        }
+    }
     
 
     
@@ -102,9 +297,11 @@ public class TernaryTreeTextFiller implements TextFiller {
      */
     private class TTNode {
         
-        boolean wordEnd;
+       // public TTNode middle;
+		boolean wordEnd;
         char letter;
         TTNode left, mid, right;
+        int queue;
         
         /**
          * Constructs a new TTNode containing the given character
@@ -113,9 +310,10 @@ public class TernaryTreeTextFiller implements TextFiller {
          * @param letter Letter to store at this node
          * @param wordEnd Whether or not this is a word-ending letter
          */
-        TTNode (char letter, boolean wordEnd) {
+        TTNode (char letter, boolean wordEnd, int queue) {
             this.letter  = letter;
             this.wordEnd = wordEnd;
+            this.queue = queue;
         }
         
     }
